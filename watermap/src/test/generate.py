48383045e72745936dba322b4caa86099dfd3052
Vedaang Verma <vermavedaang@gmail.com>
2026-03-03 21:27:44 -0500

    adjusted testing region

--- watermap/src/test/generate.py
+++ watermap/src/test/generate.py
@@ -22,7 +22,7 @@ for i in range(100):
         "turbidity_v": round(random.uniform(0.5, 10), 1),
         "ph": round(random.uniform(6.5, 10), 1),
         "lat": round(random.uniform(26.75, 27), 5),
-        "lon": round(random.uniform(-81, -80.7), 5)
+        "lon": round(random.uniform(-81.1, -80.6), 5)
     }
     entries.append(entry)
 
